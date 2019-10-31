48e05fa71fef17d548b20d916c0d75980996cf7c
Dan Fairaizl <fairaizl@gmail.com>
2019-10-31 09:17:35 -0400

    Added support for more of the LLVM C API

---
+++ index.js
@@ -0,0 +1,9 @@
+const libLLVM = require('./lib');
+const enums = require('./lib/support/enums');
+const types = require('./lib/support/types');
+
+module.exports = {
+  libLLVM,
+  enums,
+  types
+};
